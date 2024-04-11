870b338ad171d9c17f4b4a841d6937cd183e7a2b
Mike Welch <mkwelch817@gmail.com>
2024-04-11 12:58:09 -0400

    Terraform Associate Practice - ExamPro Getting Started

---
+++ 000_getting_started/providers.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  # Configuration options
+  profile = "default"
+  region = "us-east-1"
+}
+
+provider "aws" {
+  # Configuration options
+  profile = "default"
+  region = "eu-west-1"
+  alias = "eu"
+}
