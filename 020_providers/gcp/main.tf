007951335bd5b5c975da8038179881eca134ea1b
Mike Welch <mkwelch817@gmail.com>
2024-04-19 18:32:21 -0400

    Terraform Associates practice Providers (Azure and GCP)

---
+++ 020_providers/gcp/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "5.25.0"
+    }
+  }
+}
+
+provider "google" {
+  credentials = ""  // Deleted credentials file name
+  project     = "terraform-associate-practice"
+  region      = "us-central1"
+  zone        = "us-central1-c"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+      labels = {
+        my_label = "value"
+      }
+    }
+  }
+
+  network_interface {
+    network = "default"
+    access_config {}
+  }
+}
