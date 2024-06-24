4dafd588d2d9c2bcb7fccc46f048cf3b8300ecb8
Mike Welch <mkwelch817@gmail.com>
2024-06-24 15:19:23 -0400

    Terraform Associates sentinel practice

---
+++ 130_sentinel_policy/main.tf
@@ -0,0 +1,35 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "5.44.0"
+        }
+    }
+    cloud {
+        organization = "mw-terraform-practice"
+
+        workspaces {
+            name = "mw-terraform-practice-sentinel"
+        }
+    }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_vpc" "main" {
+  id = "vpc-0fac7589b76da39f2"
+}
+
+resource "aws_instance" "my_server" {
+    ami = "ami-051f8a213df8bc089"
+    instance_type = "t2.nano"
+    tags = {
+        Name = "MyServer"
+    }
+}
+
+output "public_ip" {
+  value = aws_instance.my_server.public_ip
+}
